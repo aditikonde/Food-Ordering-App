09234d341880dd420541b0bd7d41769acc9d1674
AditiKonde <aditi.konde.25@gmail.com>
2021-05-02 21:04:38 +0530

    Home page implemented with api call

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -4,19 +4,49 @@ import RestaurantCard from '../restaurantCard/RestaurantCard';
 import Header from '../../common/header/Header';
 
 class Home extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            restaurants: [{}]
+        }
+    }
+
+    componentWillMount() {
+        let data = null;
+        let url = "http://localhost:8080/api/restaurant";
+        let xhr = new XMLHttpRequest();
+        let that = this;
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                console.log(JSON.parse(this.responseText));
+                that.setState({ restaurants: JSON.parse(this.responseText).restaurants });
+            }
+        });
+
+        xhr.open("GET", url);
+        // xhr.setRequestHeader("cache-control")
+        xhr.send(data);
+    }
+
+
     render() {
         return (
             <div className="home-container">
                 <Header />
                 <div className="home-page">
+                    {this.state.restaurants.map(restaurant => (
+                        <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+                    ))}
+                    {/* <RestaurantCard />
                     <RestaurantCard />
                     <RestaurantCard />
                     <RestaurantCard />
                     <RestaurantCard />
                     <RestaurantCard />
                     <RestaurantCard />
-                    <RestaurantCard />
-                    <RestaurantCard />
+                    <RestaurantCard /> */}
+
 
                 </div>
             </div>
